bd4320b634a71668c9bbdaf44832d2ba0f2f7ad8
Santhosh-Kumar-23 <santhoshmoorthycolanonline@gmail.com>
2025-01-23 20:23:10 +0530

    updated key features

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -55,7 +55,6 @@ const Experience = () => {
 
           <div className="relative">
             {/* Timeline Line */}
-            <div className="absolute left-0 md:left-1/2 transform md:-translate-x-1/2 h-full w-1 bg-gradient-to-b from-blue-500 to-purple-500" />
 
             <div className="space-y-16">
               {experiences.map((exp, index) => (
